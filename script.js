2039da933b37403690af24b50e99e3c793c4d51a
Greg Swan <gmswan99@gmail.com>
2022-10-16 13:39:28 -0400

    Updated code button now console logs user text

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 console.log("working")
 
-
-const url = `https://opentdb.com/api.php?amount=10` //${textInput}`
-
-//reg and res
-//request and response
-//use Event as a parameter
+const url = `https://opentdb.com/api.php?amount=10` 
 
 async function getData (event) {
-    //preventing the default behavior of the browser
+  
     event.preventDefault()
 
-    fetch(`https://opentdb.com/api.php?amount=10`) //${textInput}`)
+    fetch(`https://opentdb.com/api.php?amount=10`) 
         .then(res => {
             return res.json()
         })
@@ -19,58 +14,27 @@ async function getData (event) {
             console.log("success!", res)
 
 let question = document.querySelector(`#question`)
-question.innerText = res.results[0].question
+    question.innerText = res.results[0].question
 
 let correct_answer = document.querySelector(`#answer`)
-correct_answer.innerText = res.results[0].correct_answer
+    correct_answer.innerText = res.results[0].correct_answer
 
-// if (correct_answer) (`${textInput}`) .then(res => {
-//     return Yes
-// function getChecked() {
-//     const textInput = document.getElementById('inputBar').checked;
-//     if (textInput === true) {
-//       console.log(true);
-//       } else {
-//         console.log(false);
-//     }
-//   }
 
-const textInput = () => {
-    let input = document.querySelector("#inputBar");
-    if(input.value.toLowerCase() === "correct_answer"){
-        console.log("Correct");
-    }
+    });
+
+    
 }
-
-
-
-}) 
-
-       }
-
-    //    let btnValue = document.querySelector("#btnValue")
-    //    btnValue.innerText = "Question: " + res.btnValue
-
-    
-    
-        // .catch( err => {
-        //     console.log("error!", err)
-            
- 
- 
-// trivia_category.addEventListener("click", getData)
-// trivia_difficulty.addEventListener("click", getData)
-// trivia_type.addEventListener("click", getData)
 
 let button = document.querySelector("#btnValue").addEventListener("click", getData)
 
 
-       // pokemonHeight.innerText = "Height: " + res.height
+function sayAnything() {
+    const message = document.getElementById("myText").value;
+    console.log(message);
     
-    
+    }
+    const userInput = document.getElementById("userInput")
+    userInput.addEventListener("click", sayAnything)
+    sayAnything()
+console.log(userInput);
 
- //1 Attach Event to button  
-// button.addEventListener("click", getData)
-
-
- //2 Read the input bar variable
